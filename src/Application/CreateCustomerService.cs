31c62dde2bac335c2b7c5d4e365a7fd0c55212d3
Cleber Varçal <cleber.varcal@varcalsys.com,br>
2021-11-09 01:03:29 -0300

    Configurada o producer de mensagens

--- src/Application/CreateCustomerService.cs
+++ src/Application/CreateCustomerService.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading.Tasks;
 using AutoMapper;
+using MassTransit;
 using MassTransitTutorial.Domain.Common;
 using MassTransitTutorial.Domain.Customer;
 using MassTransitTutorial.Domain.Customer.Dtos;
+using MassTransitTutorial.Domain.Events;
 
 namespace MassTransitTutorial.Application
 {
@@ -11,11 +13,13 @@ namespace MassTransitTutorial.Application
     {
         private readonly ICustomerRepository _repo;
         private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public CreateCustomerService(ICustomerRepository repo, IMapper mapper)
+        public CreateCustomerService(ICustomerRepository repo, IMapper mapper, IPublishEndpoint publishEndpoint)
         {
             _repo = repo;
             _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
         }
 
         public async Task<ServiceResult<CustomerDto>> Create(NewCustomerDto data)
@@ -24,6 +28,15 @@ namespace MassTransitTutorial.Application
             {
                 var newCustomer = _mapper.Map<NewCustomer>(data);
                 var createdCustomer = await _repo.CreateCustomer(newCustomer);
+
+                await _publishEndpoint.Publish<CustomerCreatedEvent>(new
+                {
+                    CustomerId = createdCustomer.CustomerId.Id,
+                    Name = createdCustomer.Name,
+                    BirthDate = createdCustomer.BirthDate,
+                    Type = createdCustomer.Type.ToString(),
+                    CreatedAt = createdCustomer.CreatedAt
+                });
 
                 return ServiceResult<CustomerDto>.Success(_mapper.Map<CustomerDto>(createdCustomer));
             }
---
+++ src/Domain/Events/CustomerCreatedEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransitTutorial.Domain.Customer;
+using Type = System.Type;
+
+namespace MassTransitTutorial.Domain.Events
+{
+    public interface CustomerCreatedEvent
+    {
+         string CustomerId { get; }
+         string Name { get; }
+         DateTime BirthDate { get; }
+         string Type { get; }
+         DateTime CreatedAt { get; }
+    }
+}
